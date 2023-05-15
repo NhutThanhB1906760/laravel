532216d75d6cbf9aa807eb99d0db879873255c92
NhutThanh2k1 <thanhb1906760@student.ctu.edu.vn>
2023-05-15 15:11:52 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/{id}/{name}', function (string $id, string $name) {
+    return $id. ' '.$name;
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
+Route::get('user/{id?}', function ($id = null) {
+    if (!$id) {
+        return "Xin mời nhập id";
+    }
+
+    return "User id: $id";
+});
+
+Route::prefix('users')->name('user.')->group(function () {
+    Route::get('profile', function () {
+        return 'ads';
+    })->name('profile');
+
+    Route::get('setting', function () {
+        return '12';
+    })->name('setting');
+});
